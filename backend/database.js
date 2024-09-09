069d0987bb2a9fae70db964429f743c89e819e11
pavani-edirisinghe <pavaniedirisinghe18@gmail.com>
2024-09-09 19:43:52 +0530

    second change

--- backend/database.js
+++ backend/database.js
@@ -51,7 +51,7 @@ const readTeacherInfo = async (id) => {
 }
 
 const addTeacher = async (id, name, age) => {
-    const sql = `SELECT * FROM dummyData`
+    const sql = `SELECT * FROM teacher`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
